78995adc597b97dda7f092122734dc2bb1bcf572
DESKTOP-2SEB88J\um-d40-127 <tmdgh717@naver.com>
2022-12-06 10:23:08 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술
    섹션 3. 데이터 접근 기술 - 테스트:

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -26,4 +28,24 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	/**
+	 * test 를 위한 메모리 DB 설정
+	 * test 폴더 밑에 sql 파일 생성
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 추가");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // 임베디드 모드로 동작하는 H2DB 사용가능, 뒤에는 데이터 베이스 종료 방지 설정
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+	 * 따로 이렇게 설정해주지 않으면
+	 * 스프링이 알아서 임베디드 모드로 접근하는 DataSource 를 만들어서 제공한다.. ㄷㄷㄷ
+	 * propertise에서도 DB관련 코드를 주석처리 했는데 잘 되는 걸 확인 할 수 있다.
+	 * 결론적으로 TEST 코드에 @Transactional 만 작성해주면 끝!!! 대박이다 진짜
+	 */
 }
--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -8,16 +8,38 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 스프링에서 제공하는 @Transactional 은 TEST에서 사용할 때
+ * 예외적으로 테스트가 끝나면 트랜잭션을 자동 롤백 시킨다!
+ *
+ * 1. 트랜잭션 시작
+ * 2. 테스트 실행
+ * 3. 트랜잭션 롤백
+ */
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+   /*
+   @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status; //롤백 하거나 할 때 필요해서 밖에 놔둠.
+
+    @BeforeEach
+    void beforeEach() {
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+    */
 
     @AfterEach
     void afterEach() {
@@ -25,6 +47,8 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        //트랜잭션 롤백
+        //transactionManager.rollback(status);
     }
 
     @Test
